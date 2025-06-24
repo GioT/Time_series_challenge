55ba16313368fa39303fbf5a4a97b36deb343b70
Giorgio Tamo <giorgiotamo@Giorgios-MacBook-Pro.local>
2025-06-24 09:31:25 +0200

    final push before submit

--- python/functions.py
+++ python/functions.py
@@ -3,19 +3,34 @@ import numpy as np
 import torch
 
 ## create sliding windows:
-def create_dataset(X_,y_, lookback,v=False):
+# def create_dataset(X_,y_, lookback,v=False):
+#     """Transform a time series into a prediction dataset
+#     params df X_: the raw feature dataset
+#     params df y_: the labels
+#     params int lookback: Size of window for prediction
+#     return torch.tensor x,y: the datasets in windows
+#     """
+#     X, y = [], []
+#     for i in range(X_.shape[0]-lookback):
+#         if v:
+#             print('X:',i,'-',i+lookback,'; y:',i+lookback)
+#         feature = X_.iloc[i:i+lookback]
+#         target  = y_.iloc[i:i+lookback]
+#         X.append(feature.to_numpy(dtype='float32'))
+#         y.append(target.to_numpy(dtype='float32'))
+#     return torch.tensor(X), torch.tensor(y)
+
+def create_dataset(X_, lookback,shift=0,v=False):
     """Transform a time series into a prediction dataset
-    params df X_: the raw feature dataset
-    params df y_: the labels
+    params df X_: the dataset
     params int lookback: Size of window for prediction
+    params int shift: if using windows, whether to shift labels (default 0)
     return torch.tensor x,y: the datasets in windows
     """
-    X, y = [], []
-    for i in range(X_.shape[0]-lookback):
+    X = []
+    for i in range(X_.shape[0]-lookback+shift):
         if v:
-            print('X:',i,'-',i+lookback,'; y:',i+lookback)
+            print('X:',i,'-',i+lookback)
         feature = X_.iloc[i:i+lookback]
-        target  = y_.iloc[i:i+lookback]
         X.append(feature.to_numpy(dtype='float32'))
-        y.append(target.to_numpy(dtype='float32'))
-    return torch.tensor(X), torch.tensor(y)
+    return torch.tensor(X)
